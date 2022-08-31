0c9cb4ddf10d8f6a4e113d1502f7c45cce5e30a3
Robanni <Robanniba@gmail.com>
2022-08-31 18:12:50 +0300

    Added some animation to player

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private float _speed = 4f;
     private float _jumpForce = 35f;
     private Rigidbody2D _playerRidgiedBody;
+    private Animator _playerAnimator;
     public FixedJoystick PlayerJoystick;
     public Button JumpButton;
     
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _playerRidgiedBody = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
         JumpButton.ButtonClickedEvent += PlayerJump;
     }
 
@@ -27,11 +29,15 @@ public class PlayerController : MonoBehaviour
     private void PlayerMovement()
     {
         _playerRidgiedBody.velocity = new Vector2(PlayerJoystick.Horizontal * _speed, _playerRidgiedBody.velocity.y);
+
+        _playerAnimator.SetFloat("Speed", Mathf.Abs(_playerRidgiedBody.velocity.x));
+
         PlayerFlip(PlayerJoystick.Horizontal);
     }
     private void PlayerJump()
     {
         _playerRidgiedBody.AddForce(Vector2.up * _jumpForce,ForceMode2D.Impulse);
+        _playerAnimator.SetTrigger("IsJumping");
     }
 
     private void PlayerFlip(float diractionOfFlip)
